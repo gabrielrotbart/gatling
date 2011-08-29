f11e6e153681334d707b5bb73c60310a27003584
Gabriel Rotbart <grotbart@gmail.com>
2011-08-29 22:15:07 +1000

    sniper in spec with matchers

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -2,12 +2,6 @@ require 'spec_helper'
 include Capybara::DSL
 include Magick
 
-# RSpec::Matchers.define :be_a_multiple_of do |expected|
-#   match do |actual|
-#     actual % expected == 0
-#   end
-# end
-
 RSpec::Matchers.define :look_like do |expected|
   match do |actual|
     page.driver.browser.save_screenshot('temp.png')
@@ -19,8 +13,10 @@ RSpec::Matchers.define :look_like do |expected|
     expected_img = Image.read(expected).first
     element_img = temp_screenshot.crop(location.x, location.y, size.width, size.height)    
     raise "The design reference #{expected} does not exist" unless File.exists? "google_logo.png"
-    diff_metric = element_img.compare_channel(expected_img,MeanAbsoluteErrorMetric)
-    diff_metric[1] == 0.0
+    diff_metric = element_img.compare_channel(expected_img, MeanAbsoluteErrorMetric)
+        matches = diff_metric[1] == 0.0
+        diff_metric.first.write('diff.png') unless matches
+    matches
   end
 end
 
