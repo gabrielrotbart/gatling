0e2720824200c811b680c5de11d27a85236f73dc
Gabriel Rotbart <grotbart@gmail.com>
2011-08-29 23:00:09 +1000

    cropped_element method

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -14,24 +14,21 @@ class Comparison
     page.driver.browser.save_screenshot('temp.png')
     temp_screenshot = Image.read('temp.png').first
   end
-    
-  def matches?
-    
-    #finds crop paramaters for element
-    element = @actual
-    element = element.native
+  
+  def cropped_element
+    element = @actual.native
     location = element.location
     size = element.size
-   expected_img = Image.read(@expected).first
-
+    capture.crop(location.x, location.y, size.width, size.height)
+  end
     
-    #crops image accroding to element paramaters
-    element_img = capture.crop(location.x, location.y, size.width, size.height)    
-    
-    raise "The design reference #{expected} does not exist" unless File.exists? "google_logo.png"
+  def matches?
+    cropped_element.write("cropped.png")
+    raise "The design reference #{@expected} does not exist" unless File.exists? "google_logo.png"
     
     #compares actual and expected
-    diff_metric = element_img.compare_channel(expected_img, MeanAbsoluteErrorMetric)
+    expected_img = Image.read(@expected).first
+    diff_metric = cropped_element.compare_channel(expected_img, MeanAbsoluteErrorMetric)
     matches = diff_metric[1] == 0.0
     diff_metric.first.write('diff.png') unless matches
     matches    
