23c9d91fedb26ed09d0cd1352c699a80e18aca6a
Gabriel Rotbart <grotbart@gmail.com>
2011-08-29 22:27:47 +1000

    Comparison class

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -2,21 +2,35 @@ require 'spec_helper'
 include Capybara::DSL
 include Magick
 
-RSpec::Matchers.define :look_like do |expected|
-  match do |actual|
+
+class Comparison
+
+  def initialize expected, actual
+    @expected = expected
+    @actual = actual
+  end
+  
+  def matches?
     page.driver.browser.save_screenshot('temp.png')
-    element = actual
+    element = @actual
     element = element.native
     location = element.location
     size = element.size
     temp_screenshot = Image.read('temp.png').first
-    expected_img = Image.read(expected).first
+    expected_img = Image.read(@expected).first
+    
     element_img = temp_screenshot.crop(location.x, location.y, size.width, size.height)    
     raise "The design reference #{expected} does not exist" unless File.exists? "google_logo.png"
     diff_metric = element_img.compare_channel(expected_img, MeanAbsoluteErrorMetric)
-        matches = diff_metric[1] == 0.0
-        diff_metric.first.write('diff.png') unless matches
-    matches
+    matches = diff_metric[1] == 0.0
+    diff_metric.first.write('diff.png') unless matches
+    matches    
+  end
+end
+
+RSpec::Matchers.define :look_like do |expected|
+  match do |actual|
+    Comparison.new(expected, actual).matches?
   end
 end
 
