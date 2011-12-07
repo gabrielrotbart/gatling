79a5bf87daf8215fda15944317b673f9d64b237c
Gabriel Rotbart <gabriel.rotbart@jetstar.com>
2011-12-07 18:27:56 +1100

    moving matcher to a more visible directory

---
+++ lib/gatling/look_like_matcher.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+require 'gatling'
+
+RSpec::Matchers.define :look_like do |expected|
+  match do |actual|
+    compare = Gatling::Comparison.new(expected, actual)
+    compare.matches?
+  end
+end
--- lib/gatling/version.rb
+++ lib/gatling/version.rb
@@ -1,3 +1,3 @@
 module Gatling
-  VERSION = "1.0.0"
+  VERSION = "1.0.1"
 end
