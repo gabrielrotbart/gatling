52b0ef24751d6046c98d6c134fabff6e7d98b8af
Gabriel Rotbart <gabriel.rotbart@jetstar.com>
2012-02-09 14:02:29 +1100

    new class CaptureElement handles all the capture and image manipulation outside the Comparison class

--- lib/gatling.rb
+++ lib/gatling.rb
@@ -6,7 +6,7 @@ require 'capybara/dsl'
 module Gatling
     class Comparison
       include Capybara::DSL
-      
+
       #TODO: Training mode
       #TODO: Diff with reports
       #TODO: Fuzz matches
@@ -16,36 +16,11 @@ module Gatling
         @expected = expected
         @actual = actual
 
+        @capture_element = Gatling::CaptureElement.new(@actual)
+
         @reference_image_path = Gatling::Configuration.reference_image_path
         @expected_image = "#{@reference_image_path}/#{@expected}"
         @expected_filename = "#{@expected}".sub(/\.[a-z]*/,'')
-      end
-
-
-      def capture
-        temp_dir = "#{@reference_image_path}/temp"
-        
-        FileUtils::mkdir_p(temp_dir)
-        
-        #captures the uncropped full screen
-        page.driver.browser.save_screenshot("#{temp_dir}/temp.png")
-        temp_screenshot = Magick::Image.read("#{temp_dir}/temp.png").first
-      end
-
-      def crop_element
-        element = @actual.native
-        location = element.location
-        size = element.size
-        capture.crop(location.x, location.y, size.width, size.height)
-      end
-
-      def save_crop_as_reference(cropped_element)
-        candidate_path = "#{@reference_image_path}/candidate"
-
-        FileUtils::mkdir_p(candidate_path)
-
-        cropped_element.write("#{candidate_path}/#{@expected_filename}.png")
-        candidate = "#{candidate_path}/#{@expected_filename}.png"
       end
 
       def create_diff
@@ -59,9 +34,13 @@ module Gatling
         raise "element did not match #{@expected}. A diff image: #{@expected_filename}_diff.png was created in #{diff_path}. A new reference #{candidate} can be used to fix the test"
       end
 
+      def save_element_as_reference(element)
+        candidate_path = "#{@reference_image_path}/candidate"
+        @capture_element.save_element(element, @expected_filename, candidate_path)
+      end
 
       def matches?
-        @cropped_element = crop_element
+        @cropped_element = @capture_element.crop
         if File.exists?(@expected_image)
 
           expected_img = Magick::Image.read(@expected_image).first
@@ -74,15 +53,65 @@ module Gatling
 
           matches
         else
-          candidate = save_crop_as_reference(@cropped_element)
+          candidate = save_element_as_reference(@cropped_element)
           raise "The design reference #{@expected} does not exist, #{candidate} is now available to be used as a reference. Copy candidate to root reference_image_path to use as reference"
-        end    
-      end      
+        end
+      end
     end
-    
+
+    class CaptureElement
+
+      def initialize element_to_capture
+        @reference_image_path = Gatling::Configuration.reference_image_path
+        @element = element_to_capture
+      end
+
+      def capture
+        temp_dir = "#{@reference_image_path}/temp"
+
+        FileUtils::mkdir_p(temp_dir)
+
+        #captures the uncropped full screen
+        page.driver.browser.save_screenshot("#{temp_dir}/temp.png")
+        temp_screenshot = Magick::Image.read("#{temp_dir}/temp.png").first
+      end
+
+      def crop
+        element = @element.native
+        location = element.location
+        size = element.size
+        @cropped_element = self.capture.crop(location.x, location.y, size.width, size.height)
+      end
+
+      def save_element(element, element_name, path)
+        FileUtils::mkdir_p(path)
+
+        element.write("#{path}/#{element_name}.png")
+        element = "#{path}/#{element_name}.png"
+      end
+
+
+    end
+
+    class Trainer
+
+      def initialize actual
+        super(@reference_image_path)
+        @actual = actual
+      end
+
+      def run
+        puts "-------------" + @reference_image_path
+        self.crop_element
+      end
+
+
+      #trainer will create candidates and move them to the correct folder
+    end
+
     module Configuration
 
-      class << self        
+      class << self
 
         attr_accessor 'reference_image_path'
 
@@ -95,11 +124,11 @@ module Gatling
         end
 
       end
-      
+
     end
-    
+
 end
 
 
-  
+
 
--- spec/gatling_spec.rb
+++ spec/gatling_spec.rb
@@ -1,66 +1,83 @@
 require 'spec_helper'
 include Capybara::DSL
-  
+
 
 describe 'gatling' do
-  
-  
+
+
   before(:all) do
-    
+
     include Rack::Test::Methods
 
     def app
       @app ||= Sinatra::Application
     end
-              
-    #expected image to compare with 
-    @example_good_image = 'smiley-faceicon.png'    
-      
-    @spec_support_root = spec_support_root 
+
+    #expected image to compare with
+    @example_good_image = 'smiley-faceicon.png'
+
+    @spec_support_root = spec_support_root
   end
-  
-     
-      
+
+
+
   after(:each) do
     remove_refs(@ref_path)
   end
-  
-  
+
+
   describe 'creating an initial reference (expected) image' do
-    
-  before(:each) do
-    @ref_path = Gatling::Configuration.reference_image_path = File.join(@spec_support_root, 'ref_path')
-  end  
-    
-    it "should notify that no reference exists for image and create a candidate" do
-      gatling = gatling_for_spec('smiley-faceicon.png')
-      expect {gatling.matches?}.should raise_error(RuntimeError, "The design reference #{@example_good_image} does not exist, #{@ref_path}/candidate/#{@example_good_image} is now available to be used as a reference. Copy candidate to root reference_image_path to use as reference")
-      File.exists?(File.join(@ref_path,'candidate','smiley-faceicon.png')).should be_true
-    end    
-  end
-  
-  describe 'image comparison' do
-  
-   before(:each) do
-     @ready_ref_path = Gatling::Configuration.reference_image_path = File.join(@spec_support_root, 'ready_candidate_ref')     
-   end
- 
-    it 'captured and referenced images match' do 
-      gatling = gatling_for_spec('smiley-faceicon.png')
-      gatling.matches?.should be_true
+
+    before(:each) do
+      @ref_path = Gatling::Configuration.reference_image_path = File.join(@spec_support_root, 'ref_path')
     end
-  
+
+      it "should notify that no reference exists for image and create a candidate" do
+        gatling = gatling_for_spec('smiley-faceicon.png')
+        expect {gatling.matches?}.should raise_error(RuntimeError, "The design reference #{@example_good_image} does not exist, #{@ref_path}/candidate/#{@example_good_image} is now available to be used as a reference. Copy candidate to root reference_image_path to use as reference")
+        File.exists?(File.join(@ref_path,'candidate','smiley-faceicon.png')).should be_true
+      end
+    end
+
+  describe 'image comparison' do
+
+   before(:each) do
+     @ready_ref_path = Gatling::Configuration.reference_image_path = File.join(@spec_support_root, 'ready_candidate_ref')
+     @gatling = gatling_for_spec('smiley-faceicon.png')
+   end
+
+    it 'captured and referenced images match' do
+      @gatling.matches?.should be_true
+    end
+
     # it 'captured and referenced images do NOT match' do
     #       @gatling_bad_example = Gatling::Comparison.new('smiley-bad.png', @element)
     #       expect {@gatling_bad_example.matches}.should be_true
     #     end
-    
-  end  
-  
+
+  end
+
   # MOCK SELENIUM ELEMENT
   # correct size (340px*42px)
   # image magick saves screenshot
   #create diff creates the correct candidate
-  
+
+  # describe 'training mode populates a candidate reference' do  #
+    #
+    #   before(:each) do
+    #     @ref_path = Gatling::Configuration.reference_image_path = File.join(@spec_support_root, 'ref_path')
+    #
+    #     visit('/')
+    #     element = page.find(:css, "#smiley")
+    #     @trainer = Gatling::Trainer.new(element)
+    #   end
+    #
+    #   it 'should create a reference from a candidate' do
+    #     @trainer.run
+    #     File.exists?(File.join(@ref_path,'smiley-faceicon.png')).should be_true
+    #   end
+    #
+    # end
+
 end
 
