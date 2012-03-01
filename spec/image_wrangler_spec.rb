811021cd80ef8a386541ddb8e4a0383bde7f11cd
Gabriel Rotbart <gabriel.rotbart@jetstar.com>
2012-03-01 19:59:50 +1100

    fixed spec/image_wrangler_spec.rb

--- spec/image_wrangler_spec.rb
+++ spec/image_wrangler_spec.rb
@@ -1,22 +1,24 @@
 require 'spec_helper'
 
 describe Gatling::ImageWrangler do
-  
+
+  class Point
+    attr_accessor :x, :y
+  end
+
+  class Size
+    attr_accessor :width, :height
+  end
 
   it 'should get the position of the css element' do
 
     #Overiding the stupid public method :y of YAML module
-    class OpenStruct
-      remove_method :y rescue nil
-      alias y psych_y
-      private :y
-    end
-  
-    location = OpenStruct.new
+
+    location = Point.new
     location.x = 1
     location.y = 2
 
-    size = OpenStruct.new
+    size = Size.new
     size.width = 100
     size.height = 200
 
@@ -31,7 +33,7 @@ describe Gatling::ImageWrangler do
     position[:y].should eql(2)
     position[:width].should eql(100)
     position[:height].should eql(200)
-      
+
   end
 
 end
