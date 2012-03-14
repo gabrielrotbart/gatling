09fa7445158c5bcb924dbe7bf71fdeb85d0a5de4
Mike Bain <mike@thealphatester.com>
2012-03-14 17:19:52 +1100

    Add test to check rspec matcher can fail

--- spec/rspec_matcher_spec.rb
+++ spec/rspec_matcher_spec.rb
@@ -7,29 +7,32 @@ describe 'rspec matcher' do
     @black_box = 'black.png'
     @ref_path = Gatling::Configuration.reference_image_path = File.join(spec_support_root, 'ref_path')
     create_images_for_web_page
-    create_square_image(@ref_path, 'black')
   end
 
   after(:each) do
+    ENV['GATLING_TRAINER'] = 'false'
     remove_refs(@ref_path)
   end
 
-  it 'should initialize and run gatling' do
-    visit('/fruit_app.html')
+  describe 'initializing and runnin gatling' do
 
-    black_element = page.find(:css, "#black")
+    it 'will pass if images matches refernce' do
+      create_square_image(@ref_path, 'black')
+      black_element = element_for_spec("#black")
+      black_element.should look_like(@black_box)
+    end
 
-    black_element.should look_like(@black_box)
+    it 'will fail if images dosent matches refernce' do
+      create_square_image(@ref_path, 'black')
+      red_element = element_for_spec("#red")
+      expect{red_element.should look_like(@black_box)}.should raise_error
+    end
+
+    it 'will return true if it makes a new image in trainer mode' do
+      ENV['GATLING_TRAINER'] = 'true'
+      black_element = element_for_spec("#black")
+      black_element.should look_like(@black_box)
+    end
+
   end
-
-  it 'should initialize and run training mode when GATLING_TRAINER is toggled' do
-    ENV['GATLING_TRAINER'] = 'true'
-    visit('/fruit_app.html')
-    black_element = page.find(:css, "#black")
-
-    black_element.should look_like(@black_box)
-
-    File.exists?(File.join(@ref_path,@black_box)).should be_true
-  end
-
 end
