be01f1ec73f7c5e245d1d4bbc6308e172ac7f05d
gabriel <gabriel.rotbart@jetstar.com>
2012-04-20 15:47:15 +1000

    beffing da specs

--- spec/gatling_spec.rb
+++ spec/gatling_spec.rb
@@ -31,6 +31,13 @@ describe Gatling do
       expect {subject.save_image_as_diff(@image_class_mock)}.should raise_error
     end
 
+    it "#save_image_as_candidate" do
+      @image_class_mock.should_receive(:save).with(:as => :candidate).and_return(@ref_path)
+      @image_class_mock.should_receive(:file_name).at_least(:once).and_return("some_name")
+      @image_class_mock.should_receive(:path).and_return(@path)
+      expect {subject.save_image_as_candidate(@image_class_mock)}.should raise_error
+    end
+
     describe "#save_image_as_reference" do
 
       it "when image.exists? == true" do
